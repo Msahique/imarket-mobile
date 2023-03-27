0acc35185c609a7c2a76fdf8ef3fec7e6abf549b
panshul yogesh <panshul.yogesh@microfocus.com>
2023-03-27 20:13:27 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,54 @@
+import React, {useEffect, useState} from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  ActivityIndicator,
+  PermissionsAndroid,
+  NativeModules,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+  Platform,
+  AppState,
+} from 'react-native';
+
+// Import Navigators from React Navigation
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {useFocusEffect} from '@react-navigation/native';
+
+// Import Screens
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen">
+        {/* SplashScreen which will come once for 5 Seconds */}
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          // Hiding header for Splash Screen
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
